07877d01fbdd9ad7f112b6b38ff796e60f38c875
Radu Gabriel <dinu_radu1997@yahoo.com>
2018-06-07 23:54:11 +0300

    KeyManager

---
+++ src/KeyManager.java
@@ -0,0 +1,38 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyManager implements KeyListener{
+
+	private boolean[] keys;
+	public boolean up, down, left, right, help;
+	
+	public KeyManager() {
+		keys = new boolean[256];
+	}
+	
+	public void tick() {
+		up = keys[KeyEvent.VK_W];
+		down = keys[KeyEvent.VK_S];
+		left = keys[KeyEvent.VK_A];
+		right = keys[KeyEvent.VK_D];
+		help = keys[KeyEvent.VK_H];
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+		System.out.println("Pressed");
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+	
+}
