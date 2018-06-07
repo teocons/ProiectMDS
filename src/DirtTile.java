2a55ed1f4de81ba271d08d1c4cdf4652194d0672
RadduGit <radugabriel919@gmail.com>
2018-06-07 18:54:03 -0400

    Add files via upload

--- src/DirtTile.java
+++ src/DirtTile.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class DirtTile extends Tile{
 
 	public DirtTile(int id) {
@@ -6,12 +5,3 @@ public class DirtTile extends Tile{
 	}
 
 }
-=======
-public class DirtTile extends Tile{
-
-	public DirtTile(int id) {
-		super(Assets.dirt, id);
-	}
-
-}
->>>>>>> 14557669d7293f081c910e1211d3776f3530645c
