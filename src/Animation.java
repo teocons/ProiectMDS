e49b2467df6f2e42f9d28a107625bf1eb220f262
RadduGit <radugabriel919@gmail.com>
2018-06-07 18:53:05 -0400

    Add files via upload

--- src/Animation.java
+++ src/Animation.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 public class Animation {
 	public boolean prevUp = false, prevDown = false, prevLeft = false, prevRight = false;
@@ -15,21 +14,3 @@ public class Animation {
 		prevWaitRight = 0;
 	}
 }
-=======
-
-public class Animation {
-	public boolean prevUp = false, prevDown = false, prevLeft = false, prevRight = false;
-	public int prevWaitUp = 0, prevWaitDown = 0, prevWaitLeft = 0, prevWaitRight = 0;
-	
-	public Animation() {
-		prevUp = false;
-		prevDown = false;
-		prevLeft = false;
-		prevRight = false;
-		prevWaitUp = 0;
-		prevWaitDown = 0;
-		prevWaitLeft = 0;
-		prevWaitRight = 0;
-	}
-}
->>>>>>> 14557669d7293f081c910e1211d3776f3530645c
